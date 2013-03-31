5cebd148c44a30ebb495cd05e0cd81d0e233137f
Stefan Stefanov <stefan.bg@gmail.com>
2013-03-31 20:12:28 +0300

    Fixed array dereferencing issue

--- Lib/Panel/VariablesPanel.php
+++ Lib/Panel/VariablesPanel.php
@@ -16,7 +16,18 @@ class VariablesPanel extends DebugPanel {
  * @return array
  */
 	public function beforeRender(Controller $controller) {
-		return array_merge($controller->viewVars, array('$request->data' => $controller->request->data));
+		return array_merge(self::dereference($controller->viewVars), array('$request->data' => $controller->request->data));
 	}
 
+	protected static function dereference($arr)
+	{
+	    if (is_array($arr)) {
+	        foreach ($arr as $i=>$v) {
+	            unset($arr[$i]);
+	            $arr[$i] = self::dereference($v);
+	        }
+	    }
+	     
+	    return $arr;
+	}
 }
